a5fa53126e6696c03af68ce3512083e24209ebd2
eotodcp <eotodcp@gmail.com>
2022-06-22 13:04:03 +0900

    asdf

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   const [message, setMessage] = useState('')
 
   useEffect(() => {
-    window.addEventListener('message', ({data}) => {
+    document.addEventListener('message', ({data}) => {
       setMessage(data)
     })
 
