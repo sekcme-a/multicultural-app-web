ec1846875a3a3975fc895b1874795806c653ccfa
eotodcp <eotodcp@gmail.com>
2022-07-08 05:59:30 +0900

    asdf

--- src/components/main/ThumbnailPost.js
+++ src/components/main/ThumbnailPost.js
@@ -24,7 +24,7 @@ const ThumbnailPost = (props) => {
             </p>
             <BookmarkBorderIcon className={styles.icon} />
           </div>
-          <Image src={props.data.thumbnail} alt={props.data.title} layout="fill" objectFit="cover" objectPosition="center" lazyBoundary="60000px"/>
+          <Image src={props.data.thumbnail} alt={props.data.title} layout="fill" objectFit="cover" objectPosition="center" priority={true} />
         </div>
         <div className={styles.footer_container}>
           <h2>{props.data.title}</h2>
